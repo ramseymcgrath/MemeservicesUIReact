ae31128d5562124e200d5a0489c1752a4a21e8ba
ramsey_mcgrath <ramsey.mcgrath@paddypowerbetfair.com>
2020-07-07 18:40:09 -0700

    Header formatted

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ import Navigation from "./Navigation";
 const Header = ({ history, handleSubmit }) => {
   return (
     <div>
-      <h1>SnapShot</h1>
+      <h1>Memeservices</h1>
+      <h2>The Home of Gourmet Memes</h2>
       <Form history={history} handleSubmit={handleSubmit} />
       <Navigation />
     </div>
